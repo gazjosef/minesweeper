2f4bd7a8f19b92e0d6e1e7217fa3b3dffbc67f94
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-29 22:34:54 +1000

    Added Minesweeper Project

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 document.addEventListener('DomContentLoaded', () => {
   const grid = document.querySelector('.grid');
+  let width = 10;
+  let squares = [];
+
+  // Create Board
+  function createBoard() {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement('div');
+      square.setAttribute('id', i);
+      grid.appendChild(square);
+    }
+  }
 });
