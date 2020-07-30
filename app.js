274f3b43b32b13524327b4e736f63eed6c14a27b
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-30 21:49:06 +1000

    Updated Minesweeper Project

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Create Board
   function createBoard() {
+    // Get Shuffled Game Array w/ Random Bombs
+
     for (let i = 0; i < width * width; i++) {
       const square = document.createElement('div');
       square.setAttribute('id', i);
