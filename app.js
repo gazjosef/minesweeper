30605d78a39533d63248fc9873139f72cea01cb5
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-31 00:57:38 +1000

    Updated Minesweeper Project

--- app.js
+++ app.js
@@ -9,16 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // Get Shuffled Game Array w/ Random Bombs
     const bombsArray = Array(bombAmount).fill('bomb');
     const emptyArray = Array(width * width - bombAmount).fill('valid');
-    console.log(bombsArray);
-    console.log(emptyArray);
     const gameArray = emptyArray.concat(bombsArray);
-    console.log(gameArray);
     const shuffleArray = gameArray.sort(() => Math.random() - 0.5);
-    console.log(shuffleArray);
 
     for (let i = 0; i < width * width; i++) {
       const square = document.createElement('div');
       square.setAttribute('id', i);
+      square.classList.add(shuffleArray[i]);
       grid.appendChild(square);
       squares.push(square);
     }
