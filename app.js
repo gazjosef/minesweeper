363c5b9781f51a1708da5ee011bf0da761669327
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-08-05 20:46:54 +1000

    Updated Minesweeper Project

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // Add Numbers
     for (let i = 0; i < squares.length; i++) {
       const isLeftEdge = i % width === 0;
-      const isRightEdge = i === width - 1;
+      const isRightEdge = i % width === width - 1;
 
       if (squares[i].classList.contains('valid')) {
-        if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) {
-        }
+        if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb'))
+          total++;
       }
     }
   }
