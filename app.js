ef4670310bac3ab268c1829632c8cd3d30cf59f0
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-08-09 18:53:50 +1000

    Updated Minesweeper Project

--- app.js
+++ app.js
@@ -103,6 +103,48 @@ document.addEventListener('DOMContentLoaded', () => {
         const newSquare = document.getElementById(newId);
         click(newSquare);
       }
+
+      if (currentId > 9 && !isRightEdge) {
+        const newId = squares[parseInt(currentId) + 1 - width].id;
+        const newSquare = document.getElementById(newId);
+        click(newSquare);
+      }
+
+      if (currentId > 10) {
+        const newId = squares[parseInt(currentId - width)].id;
+        const newSquare = document.getElementById(newId);
+        click(newSquare);
+      }
+
+      if (currentId > 11 && !isLeftEdge) {
+        const newId = squares[parseInt(currentId) - 1 - width].id;
+        const newSquare = document.getElementById(newId);
+        click(newSquare);
+      }
+
+      if (currentId < 98 && !isRightEdge) {
+        const newId = squares[parseInt(currentId) + 1].id;
+        const newSquare = document.getElementById(newId);
+        click(newSquare);
+      }
+
+      if (currentId < 90 && !isLeftEdge) {
+        const newId = squares[parseInt(currentId) - 1 + width].id;
+        const newSquare = document.getElementById(newId);
+        click(newSquare);
+      }
+
+      if (currentId < 88 && !isRightEdge) {
+        const newId = squares[parseInt(currentId) + 1 + width].id;
+        const newSquare = document.getElementById(newId);
+        click(newSquare);
+      }
+
+      if (currentId < 89) {
+        const newId = squares[parseInt(currentId) + width].id;
+        const newSquare = document.getElementById(newId);
+        click(newSquare);
+      }
     }, 10);
   }
 });
